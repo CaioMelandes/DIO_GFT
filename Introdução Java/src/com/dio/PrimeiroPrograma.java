423dbc02bcc8ce96177600db8da09c8115186060
CaioMelandes <caio.melandes@gmail.com>
2022-02-18 17:24:58 -0300

    Update

--- Introdução Java/src/com/dio/PrimeiroPrograma.java
+++ Introdução Java/src/com/dio/PrimeiroPrograma.java
@@ -6,7 +6,8 @@ public class PrimeiroPrograma {
 
     public static void main(String[] args) {
         Ordem ordem = new Ordem("code1234", 99);
+        double valorFinal = ordem.calcularTaxa();
         System.out.println(ordem);
+        System.out.println(valorFinal);
     }
-
 }
