edd43a1d25c10f949a1a0d0a189851f2a92ca4a9
CaioMelandes <caio.melandes@gmail.com>
2022-02-18 17:24:44 -0300

    Apresentação da IDE

---
+++ Introdução Java/src/com/dio/IDE.java
@@ -0,0 +1,11 @@
+package com.dio;
+
+import com.dio.base.Gato;
+
+public class IDE {
+    public static void main(String[] args) {
+        Gato gato = new Gato("Leona", "Preta e Laranja", 3);
+
+        System.out.println(gato);
+    }
+}
